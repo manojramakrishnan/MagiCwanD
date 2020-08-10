2bd9d13645549006c2d5d5f662f6868be3ac16a6
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2020-08-10 16:06:11 +0530

    registration fetch issue resolved

--- src/main/java/com/magicwand/repository/UserRepository.java
+++ src/main/java/com/magicwand/repository/UserRepository.java
@@ -9,7 +9,7 @@ import com.magicwand.entity.Registration;
 
 public interface UserRepository extends CrudRepository<Registration,Integer> {
 	
-	@Query("select r from Registration r join Plan p where p.plan_id=r.planid and r.id=:id")
+	@Query("select r from Registration r, Plan p where p.plan_id=r.planid and r.id=:id")
 	public List<Registration> findByReg_Id(Integer id); 
 }
 
